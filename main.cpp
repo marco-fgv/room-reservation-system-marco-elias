20237c6380e79afb5eb0fee7ec205b56e8582723
Marco Tulio Silva <marco.tps437@gmail.com>
2026-03-28 12:08:42 -0300

    alteraçoes no main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main(){
     int capacidades[3] = {100, 200, 300};
-    Dia dias[5] = {Dia("Segunda"), Dia("Terça"), Dia("Quarta"), Dia("Quinta"), Dia("Sexta")};
 
     ReservationSystem sistema(3, capacidades);
 
